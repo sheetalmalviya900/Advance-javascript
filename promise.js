814181e7243d268909d79e0d2ec13e38548cfffa
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2022-08-26 12:02:41 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,13 +1,13 @@
 // Program with a Promise
-// const count = true;
-// let countValue = new Promise(function (resolve, reject) {
-//     if (count) {
-//         resolve("There is a count value.");
-//     } else {
-//         reject("There is no count value");
-//     }
-// });
-// console.log(countValue);
+const count = true;
+let countValue = new Promise(function (resolve, reject) {
+    if (count) {
+        resolve("There is a count value.");
+    } else {
+        reject("There is no count value");
+    }
+});
+console.log(countValue);
 
 
 
@@ -28,94 +28,104 @@ let countValue = new Promise(function (resolve, reject) {
 
 
 
-// // JavaScript catch() method
-// // returns a promise
-// let countValue = new Promise(function (resolve, reject) {
-//     reject('Promise rejected'); 
-//  });
+
+
+// JavaScript catch() method
+// returns a promise
+let countValue = new Promise(function (resolve, reject) {
+    reject('Promise rejected'); 
+ });
  
-//  // executes when promise is resolved successfully
-//  countValue.then(
-//      function successValue(result) {
-//          console.log(result);
-//      },
-//   )
+ // executes when promise is resolved successfully
+ countValue.then(
+     function successValue(result) {
+         console.log(result);
+     },
+  )
  
-//  // executes if there is an error
-//  .catch(
-//      function errorValue(result) {
-//          console.log(result);
-//      }
-//  );
-
-
-
-// // JavaScript finally() method
-// // returns a promise
-// let countValue = new Promise(function (resolve, reject) {
-//     // could be resolved or rejected   
-//     resolve('Promise resolved'); 
-// });
-// // add other blocks of code
-// countValue.finally(
-//     function greet() {
-//         console.log('This code is executed.');
-//     }
-// );
-
-
-
-// var promise = new Promise(function(resolve, reject) {
-//     const x = "geeksforgeeks";
-//     const y = "geeksforgeeks"
-//     if(x === y) {
-//         resolve();
-//     } else {
-//         reject();
-//     }
-//     });
-//     promise.
-//         then(function () {
-//             console.log('Success, You are a GEEK');
-//         }).
-//         catch(function () {
-//             console.log('Some error has occurred');
-//         });
+ // executes if there is an error
+ .catch(
+     function errorValue(result) {
+         console.log(result);
+     }
+ );
 
 
 
 
 
-// var promise = new Promise(function(resolve, reject) {
-//     const x = "I AM SOFTWARE DEVLOPER";
-//     const y = "I WAS START MY CORDING JOURNEY FROM NAVGURUKUL"
-//     if(x === y) {
-//         resolve();
-//     } else {
-//         reject();
-//     }
-//     });
-//     promise.
-//         then(function () {
-//             console.log('Success, You are a SOFTWARE DEVLOPER');
-//         }).
-//         catch(function () {
-//             console.log('Some error has occurred');
-//         });
+// JavaScript finally() method
+// returns a promise
+let countValue = new Promise(function (resolve, reject) {
+    // could be resolved or rejected   
+    resolve('Promise resolved'); 
+});
+// add other blocks of code
+countValue.finally(
+    function greet() {
+        console.log('This code is executed.');
+    }
+);
 
-//  function f1() {
-    //     console.log('f1');
-    // }
-    // function f2() {
-    //     console.log('f2');
-    // }
-    // function main() {
-    //     console.log('main');
-    //     setTimeout(f1, 1000);
+
+
+
+
+var promise = new Promise(function(resolve, reject) {
+    const x = "geeksforgeeks";
+    const y = "geeksforgeeks"
+    if(x === y) {
+        resolve();
+    } else {
+        reject();
+    }
+    });
+    promise.
+        then(function () {
+            console.log('Success, You are a GEEK');
+        }).
+        catch(function () {
+            console.log('Some error has occurred');
+        });
+
+
+
+
+
+var promise = new Promise(function(resolve, reject) {
+    const x = "I AM SOFTWARE DEVLOPER";
+    const y = "I WAS START MY CORDING JOURNEY FROM NAVGURUKUL"
+    if(x === y) {
+        resolve();
+    } else {
+        reject();
+    }
+    });
+    promise.
+        then(function () {
+            console.log('Success, You are a SOFTWARE DEVLOPER');
+        }).
+        catch(function () {
+            console.log('Some error has occurred');
+        });
+
+
+
+
+
+ function f1() {
+        console.log('f1');
+    }
+    function f2() {
+        console.log('f2');
+    }
+    function main() {
+        console.log('main');
+        setTimeout(f1, 1000);
         
-    //     new Promise((resolve, reject) =>
-    //         resolve('I am a promise')
-    //     ).then(resolve => console.log(resolve))
-    //     f2();
-    // }
-    // main();
+        new Promise((resolve, reject) =>
+            resolve('I am a promise')
+        ).then(resolve => console.log(resolve))
+        f2();
+    }
+    main();
