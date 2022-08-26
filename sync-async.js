48d00229060d0614906ce49ea85fb1227423e7f7
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2022-08-26 11:57:26 +0530

    Update sync-async.js

--- sync-async.js
+++ sync-async.js
@@ -1,56 +1,56 @@
-// synchronus
-// function makeGreeting(name) {
-//     return `Hello, my name is ${name}!`;
-// }
-// const name = 'Miriam';
-// const greeting = makeGreeting(name);
-// console.log(greeting);
-// // "Hello, my name is Miriam!"
+synchronus
+function makeGreeting(name) {
+    return `Hello, my name is ${name}!`;
+}
+const name = 'Miriam';
+const greeting = makeGreeting(name);
+console.log(greeting);
+// "Hello, my name is Miriam!"
   
 
 
-// // asynchronus
-// setTimeout(() => {
-//     for(let index=0;index<4;index++){
-//         // element=0;
-//         console.log("this is index number"+index);
-//     }
-// }, 1000);
-// console.log("done printing")
+// asynchronus
+setTimeout(() => {
+    for(let index=0;index<4;index++){
+        // element=0;
+        console.log("this is index number"+index);
+    }
+}, 1000);
+console.log("done printing")
 
 
 
-// console.log('One');
-// setTimeout(() => console.log('Two'), 1000);
-// console.log('Three');
-// // LOGS: 'One', 'Three', 'Two'
+console.log('One');
+setTimeout(() => console.log('Two'), 1000);
+console.log('Three');
+// LOGS: 'One', 'Three', 'Two'
 
 
 
-// function printMe() {
-//     console.log('print me');
-// }
-// function test() {
-// console.log('test');
-// }
-// setTimeout(printMe, 2000);
-// test();
+function printMe() {
+    console.log('print me');
+}
+function test() {
+console.log('test');
+}
+setTimeout(printMe, 2000);
+test();
 
 
 
-// function f1() {
-//     console.log('f1');
-// }
-// function f2() {
-//     console.log('f2');
-// }
-// function main() {
-//     console.log('main');
-//     setTimeout(f1, 1000);
+function f1() {
+    console.log('f1');
+}
+function f2() {
+    console.log('f2');
+}
+function main() {
+    console.log('main');
+    setTimeout(f1, 1000);
     
-//     new Promise((resolve, reject) =>
-//         resolve('I am a promise')
-//     ).then(resolve => console.log(resolve))
-//     f2();
-// }
-// main();
+    new Promise((resolve, reject) =>
+        resolve('I am a promise')
+    ).then(resolve => console.log(resolve))
+    f2();
+}
+main();
