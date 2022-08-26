184e7904f130ce88aefea7193c638dd93b100ae0
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2022-08-26 11:45:18 +0530

    Update Async_await.js

--- Async_await.js
+++ Async_await.js
@@ -1,37 +1,37 @@
-// // async function example
-// async function f() {
-//     console.log('Async function.');
-//     return Promise.resolve(1);
-// }
-//
-
-// async function f() {
-//     console.log('Async function.');
-//     return Promise.resolve(1);
-// }
-// f().then(function(result) {
-//     console.log(result)
-// });
+// async function example
+async function f() {
+    console.log('Async function.');
+    return Promise.resolve(1);
+}
 
 
+async function f() {
+    console.log('Async function.');
+    return Promise.resolve(1);
+}
+f().then(function(result) {
+    console.log(result)
+});
 
-// // Error Handling
-// // a promise
-// let promise = new Promise(function (resolve, reject) {
-//     setTimeout(function () {
-//     resolve('Promise resolved')}, 4000); 
-// });
-// // async function
-// async function asyncFunc() {
-//     try {
-//         // wait until the promise resolves 
-//         let result = await promise; 
 
-//         console.log(result);
-//     }   
-//     catch(error) {
-//         console.log(error);
-//     }
-// }
-// // calling the async function
-// asyncFunc(); // Promise resolved
+
+// Error Handling
+// a promise
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+    resolve('Promise resolved')}, 4000); 
+});
+// async function
+async function asyncFunc() {
+    try {
+        // wait until the promise resolves 
+        let result = await promise; 
+
+        console.log(result);
+    }   
+    catch(error) {
+        console.log(error);
+    }
+}
+// calling the async function
+asyncFunc(); // Promise resolved
