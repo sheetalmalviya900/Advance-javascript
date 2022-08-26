f826e0516310761e90c743e9a23073f1bdd8643a
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2022-08-26 11:58:47 +0530

    Update setTimeOut_timeinterval.js

--- setTimeOut_timeinterval.js
+++ setTimeOut_timeinterval.js
@@ -1,121 +1,121 @@
-// // Display a Text Once After 3 Second
-// // program to display a text using setTimeout method
-// function greet() {
-//     console.log('Hello world');
-// }
-// setTimeout(greet, 3000);
-// console.log('This message is shown first');
+// Display a Text Once After 3 Second
+// program to display a text using setTimeout method
+function greet() {
+    console.log('Hello world');
+}
+setTimeout(greet, 3000);
+console.log('This message is shown first');
 
 
 
-// // Program with setTimeout()
-// //  program that shows the delay in execution
-// function greet() {
-//     console.log('Hello world');
-// }
-// function sayName(name) {
-//     console.log('Hello' + ' ' + name);
-// }
-// // calling the function
-// setTimeout(greet, 2000);
-// sayName('John');
+// Program with setTimeout()
+//  program that shows the delay in execution
+function greet() {
+    console.log('Hello world');
+}
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+// calling the function
+setTimeout(greet, 2000);
+sayName('John');
 
 
 
-// // The setTimeout() method returns the interval id. 
-// // program to display a text using setTimeout method
-// function greet() {
-//     console.log('Hello world');
-// }
-// let intervalId = setTimeout(greet, 3000);
-// console.log('Id: ' + intervalId);  
+// The setTimeout() method returns the interval id. 
+// program to display a text using setTimeout method
+function greet() {
+    console.log('Hello world');
+}
+let intervalId = setTimeout(greet, 3000);
+console.log('Id: ' + intervalId);  
 
 
 
-// // Display Time Every 3 Second
-// // program to display time every 3 seconds
-// function showTime() {
+// Display Time Every 3 Second
+// program to display time every 3 seconds
+function showTime() {
 
-//     // return new date and time
-//     let dateTime= new Date();
+    // return new date and time
+    let dateTime= new Date();
 
-//     // returns the current local time
-//     let time = dateTime.toLocaleTimeString();
+    // returns the current local time
+    let time = dateTime.toLocaleTimeString();
 
-//     console.log(time)
+    console.log(time)
 
-//     // display the time after 3 seconds
-//      setTimeout(showTime, 1000);
-// }
-// // calling the function
-// showTime();
-
-
-
-// // Use clearTimeout() Method
-// // program to stop the setTimeout() method
-// let count = 0;
-// // function creation
-// function increaseCount(){
-
-//     // increasing the count by 1
-//     count += 1;
-//     console.log(count)
-// }
-// let id = setTimeout(increaseCount, 3000);
-// // clearTimeout
-// // increaseCount()
-// clearTimeout(id); 
-// console.log('setTimeout is stopped.');
+    // display the time after 3 seconds
+     setTimeout(showTime, 1000);
+}
+// calling the function
+showTime();
 
 
 
-// // program to display a name
-// function greet(name, lastName) {
-//     console.log('Hello' + ' ' + name + ' ' + lastName);
-// }
-// // passing argument to setTimeout
-// setTimeout(greet, 1000, 'John', 'Doe');
+// Use clearTimeout() Method
+// program to stop the setTimeout() method
+let count = 0;
+// function creation
+function increaseCount(){
+
+    // increasing the count by 1
+    count += 1;
+    console.log(count)
+}
+let id = setTimeout(increaseCount, 3000);
+// clearTimeout
+// increaseCount()
+clearTimeout(id); 
+console.log('setTimeout is stopped.');
 
 
 
-// // Display Time Every 5 Seconds
-// // program to display time every 5 seconds
-// function showTime() {
-
-//     // return new date and time
-//     let dateTime= new Date();
-
-//     // return the time
-//     let time = dateTime.toLocaleTimeString();
-
-//     console.log(time)
-// }
-// let display = setInterval(showTime, 5000);
+// program to display a name
+function greet(name, lastName) {
+    console.log('Hello' + ' ' + name + ' ' + lastName);
+}
+// passing argument to setTimeout
+setTimeout(greet, 1000, 'John', 'Doe');
 
 
 
+// Display Time Every 5 Seconds
+// program to display time every 5 seconds
+function showTime() {
 
-// // Use clearInterval() Method
-// // program to stop the setInterval() method after five times
-// let count = 0;
-// // function creation
-// let interval = setInterval(function(){
+    // return new date and time
+    let dateTime= new Date();
 
-//     // increasing the count by 1
-//     count += 1;
+    // return the time
+    let time = dateTime.toLocaleTimeString();
 
-//     // when count equals to 5, stop the function
-//     if(count === 5){
-//         clearInterval(interval);
-//     }
+    console.log(time)
+}
+let display = setInterval(showTime, 5000);
 
-//     // display the current time
-//     let dateTime= new Date();
-//     let time = dateTime.toLocaleTimeString();
-//     console.log(time);
 
-// }, 2000);
+
+
+// Use clearInterval() Method
+// program to stop the setInterval() method after five times
+let count = 0;
+// function creation
+let interval = setInterval(function(){
+
+    // increasing the count by 1
+    count += 1;
+
+    // when count equals to 5, stop the function
+    if(count === 5){
+        clearInterval(interval);
+    }
+
+    // display the current time
+    let dateTime= new Date();
+    let time = dateTime.toLocaleTimeString();
+    console.log(time);
+
+}, 2000);
 
 
 
