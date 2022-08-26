e9ee295296042ec747254c24b229a12a5314447a
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2022-08-26 12:13:09 +0530

    Update crud_operation.js

--- crud_operation.js
+++ crud_operation.js
@@ -1,9 +1,9 @@
-// const fs=require("fs");
-// fs.mkdirSync("AARTI") //for creating folder
-// fs.writeFileSync("AARTI/bio.txt","my name is aarti kumari i am from delhi."); //create a file
-// fs.appendFileSync("AARTI/bio.txt","i have completed my graduation from delhi univercity."); //for add more data
-// const data=fs.readFileSync("AARTI/bio.txt","utf-8"); // for read the file
-// console.log(data);
-// fs.renameSync("AARTI/bio.txt","AARTI/AATU.txt"); //for rename the file
-// fs.unlinkSync("AARTI/AATU.txt"); //for delete the file
-// fs.rmdirSync("AARTI"); //for delete the folder
+const fs=require("fs");
+fs.mkdirSync("AARTI") //for creating folder
+fs.writeFileSync("AARTI/bio.txt","my name is aarti kumari i am from delhi."); //create a file
+fs.appendFileSync("AARTI/bio.txt","i have completed my graduation from delhi univercity."); //for add more data
+const data=fs.readFileSync("AARTI/bio.txt","utf-8"); // for read the file
+console.log(data);
+fs.renameSync("AARTI/bio.txt","AARTI/AATU.txt"); //for rename the file
+fs.unlinkSync("AARTI/AATU.txt"); //for delete the file
+fs.rmdirSync("AARTI"); //for delete the folder
